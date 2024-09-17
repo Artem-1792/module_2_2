6bcfbba8bc36098c702f2e6cb0cc6a1156738d04
Артём Салимов <arsa-17@bk.ru>
2024-09-17 22:51:26 +0500

    Add files via upload

---
+++ module_2_2.py
@@ -0,0 +1,15 @@
+#Задача "Все ли равны?":
+
+first=123
+second=124
+third=125
+
+if  first == second or first == third or second == third:
+    print(2)
+
+elif first == second == third:
+    print(3)
+
+else:
+    print(0)
+
